48f8c44b4a11129f05df7a880fdebf8a118aee7d
AnjaliSavalkar <savalkaranjali6@gmail.com>
2025-04-11 13:41:15 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -378,7 +378,7 @@ app.post("/upload", upload.single("image"), (req, res) => {
 
   res.json({
     success: true,
-    image_url: `http://localhost:${port}/images/${req.file.filename}`,
+    image_url: `https://ecommerce-full-backend.onrender.com/images/${req.file.filename}`,
   });
 });
 
